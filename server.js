411d1e910347a15b787a5c71e1ec0191ad7272aa
Ohioze Omiunu Alex <ohiozeomiunu@gmail.com>
2023-09-08 13:41:46 +0100

    updates

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ app.get("/api", (req, res) => {
   const slackName = req.query.slack_name || "Alex Omiunu";
   const track = req.query.track || "backend";
   const currentDate = new Date();
+  const year = currentDate.getUTCFullYear();
+  const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getUTCDate()).padStart(2, "0");
+  const hours = String(currentDate.getUTCHours()).padStart(2, "0");
+  const minutes = String(currentDate.getUTCMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getUTCSeconds()).padStart(2, "0");
+  const utcTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
   const currentDay = currentDate.toLocaleDateString("en-US", {
     weekday: "long",
   });
-  const utcTime = currentDate.toISOString();
+  //   const utcTime = currentDate.toISOString();
 
   const githubFileUrl =
     "https://github.com/alxzndr2019/hngx-stage1/blob/main/server.js";
